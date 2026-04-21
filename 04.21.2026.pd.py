6c500b0e9b370b6045e866fe9d057d3bc8a14c4e
lazy-gooner <ronmartinov0@gmail.com>
2026-04-21 08:49:30 +0300

    Add files via upload

---
+++ 04.21.2026.pd.py
@@ -0,0 +1,17 @@
+import json
+
+with open("uzd2.json","r",encoding="utf-8")as datne:
+  dati=json.load(datne)
+pilsētas=["Rīga","Daugavpils","Liepāja","Jelgava","Jūrmala","Ventspils","Rēzekne","Valmiera","Ogre","Jēkabpils"]
+pensionāru_īpastsvars=[]
+for pilsētas in pensionāru_īpastsvars:
+
+ print()
+ for procentos in pensionāru_īpastsvars:
+  procentos.value()
+  with open("rezultati.csv", "w", encoding="utf-8") as save:
+print(procentos)
+
+    
+
+
