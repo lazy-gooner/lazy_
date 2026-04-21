82791fdcc828467e336f60b714d090e6e7f953f7
lazy-gooner <ronmartinov0@gmail.com>
2026-04-21 09:47:43 +0300

    Update 04.21.2026.pd.py

--- 04.21.2026.pd.py
+++ 04.21.2026.pd.py
@@ -3,14 +3,16 @@ import csv
 
 with open("uzd2.json","r",encoding="utf-8")as datne:
   dati=json.load(datne)
-pilsētas=["Rīga","Daugavpils","Liepāja","Jelgava","Jūrmala","Ventspils","Rēzekne","Valmiera","Ogre","Jēkabpils"]
-pensionāru_īpastsvars=[]
-for pilsētas in pensionāru_īpastsvars:
-with open("rezultati.csv", "w", encoding="utf-8") as save:
-  rakstitajs = csv.writer(save)
-  rakstitajs.writerow(["Pilsēta", "pensionāru_īpastsvars"( %)"])
-  for i in range(len(pilsētas)):
-    pilsētas = pensionāru_īpastsvars[i]
+pilsetas=["Rīga","Daugavpils","Liepāja","Jelgava","Jūrmala","Ventspils","Rēzekne","Valmiera","Ogre","Jēkabpils"]
+pensionaru_ipastsvars=[]
+for pilsetas in pensionaru_ipastsvars:
+ with open("rezultati.csv", "w", encoding="utf-8") as save:
+  procentos = csv.writer(save)
+  procentos.writerow(["Pilsēta", "pensionāru_īpastsvars"])
+  for i in range(len(pilsetas)):
+    pilsetas = pensionaru_ipastsvars[i]
+    print()
+    Normunda Zujeva darbs
     
 
 
